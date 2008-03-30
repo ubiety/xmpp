bb5f801008fefb7dd3b7227cc3ac3d4f22a058c9
Dieter Lunn <coder2000@gmail.com>
2008-03-30 04:57:27 +0000

    Removed unneeded xml classes.

--- xmpp/Properties/AssemblyInfo.cs
+++ xmpp/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("xmpp")]
-[assembly: AssemblyCopyright("Copyright © 2006 Dieter Lunn")]
+[assembly: AssemblyCopyright("Copyright © 2006 - 2008 Dieter Lunn")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
