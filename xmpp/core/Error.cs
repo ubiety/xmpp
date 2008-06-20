26f61f962b0e7c8b6ff418b1802dfdc59bc04b6c
Dieter Lunn <coder2000@gmail.com>
2008-06-20 06:29:13 +0000

    Started adding error tags

---
+++ xmpp/core/Error.cs
@@ -0,0 +1,29 @@
+// Error.cs
+//
+//XMPP .NET Library Copyright (C) 2008 Dieter Lunn
+//
+//This library is free software; you can redistribute it and/or modify it under
+//the terms of the GNU Lesser General Public License as published by the Free
+//Software Foundation; either version 3 of the License, or (at your option)
+//any later version.
+//
+//This library is distributed in the hope that it will be useful, but WITHOUT
+//ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+//FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+//
+//You should have received a copy of the GNU Lesser General Public License along
+//with this library; if not, write to the Free Software Foundation, Inc., 59
+//Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+using xmpp.attributes;
+
+namespace xmpp
+{
+	[XmppTag("stream", "", typeof(Error))]
+	public class Error
+	{		
+		public Error()
+		{
+		}
+	}
+}
