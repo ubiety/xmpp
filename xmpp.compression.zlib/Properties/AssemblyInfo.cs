aed4e3b5688d0c705c99ad11be08e366cc4c89ff
coder2000 <coder2000@gmail.com>
2009-08-28 11:31:47 -0500

    Updated VS2008 files

--- xmpp.compression.zlib/Properties/AssemblyInfo.cs
+++ xmpp.compression.zlib/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ubiety.compression.zlib")]
-[assembly: AssemblyCopyright("Copyright © 2008 Dieter Lunn")]
+[assembly: AssemblyCopyright("Copyright © 2008 - 2009 Dieter Lunn")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("0.1.*")]
-[assembly: AssemblyFileVersion("0.1.*")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
--- xmpp/Properties/AssemblyInfo.cs
+++ xmpp/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ubiety")]
-[assembly: AssemblyCopyright("Copyright © 2006 - 2008 Dieter Lunn")]
+[assembly: AssemblyCopyright("Copyright © 2006 - 2009 Dieter Lunn")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("0.2.*")]
-[assembly: AssemblyFileVersion("0.2.*")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
