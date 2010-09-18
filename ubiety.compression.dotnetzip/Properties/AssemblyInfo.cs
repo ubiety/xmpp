fe7508973cd69e0e4746ace19d2a67a426438da2
Dieter Lunn <coder2000@gmail.com>
2010-09-18 16:00:12 -0500

    Updated all assembly versions

--- ubiety.compression.dotnetzip/Properties/AssemblyInfo.cs
+++ ubiety.compression.dotnetzip/Properties/AssemblyInfo.cs
@@ -17,7 +17,8 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyVersion("0.1.918.1")]
+[assembly: AssemblyFileVersion("0.3.918.3")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
