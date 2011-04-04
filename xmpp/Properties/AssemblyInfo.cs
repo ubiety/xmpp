c26bcd13b3edbed6ac0ac7923ba0f8a2c6698749
Dieter Lunn <coder2000@gmail.com>
2011-04-03 19:32:00 -0500

    Minor changes and finally have the relocated references added.

--- xmpp/Properties/AssemblyInfo.cs
+++ xmpp/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.5.0328.129")]
-[assembly: AssemblyFileVersion("0.5.0328.129")]
+[assembly: AssemblyVersion("0.5.0403.130")]
+[assembly: AssemblyFileVersion("0.5.0403.130")]
