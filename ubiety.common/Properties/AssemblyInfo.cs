75078b10ea098abe85f08db9d4bdce5599fc56f6
Dieter Lunn <coder2000@gmail.com>
2012-09-15 10:05:46 -0500

    Updated version

--- ubiety.common/Properties/AssemblyInfo.cs
+++ ubiety.common/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ubiety.common")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Ubiety XMPP Common")]
+[assembly: AssemblyDescription("Common classes used across all ubiety libraries.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ubiety.common")]
-[assembly: AssemblyCopyright("Copyright ©  2011")]
+[assembly: AssemblyCopyright("Copyright © 2006 - 2012 Dieter Lunn")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.5.0915.170")]
+[assembly: AssemblyFileVersion("0.5.0915.170")]
--- ubiety/Properties/AssemblyInfo.cs
+++ ubiety/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ubiety")]
-[assembly: AssemblyCopyright("Copyright © 2006 - 2011 Dieter Lunn")]
+[assembly: AssemblyCopyright("Copyright © 2006 - 2012 Dieter Lunn")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.5.1214.163")]
-[assembly: AssemblyFileVersion("0.5.1214.163")]
+[assembly: AssemblyVersion("0.5.0915.170")]
+[assembly: AssemblyFileVersion("0.5.0915.170")]
