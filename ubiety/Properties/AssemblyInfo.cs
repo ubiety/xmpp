71884ec52f5c9ca81ce8a7dfd6e7cf65035cc5a2
Dieter Lunn <coder2000@gmail.com>
2017-04-18 14:50:34 -0500

    Implement GitVersion versioning

--- ubiety/Properties/AssemblyInfo.cs
+++ ubiety/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ubiety")]
-[assembly: AssemblyCopyright("Copyright © 2006 - 2015 Dieter Lunn")]
+[assembly: AssemblyCopyright("Copyright © 2006 - 2017 Dieter Lunn")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,15 +20,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("6f3bc432-dec4-44c2-9863-2c6c61dfee76")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("0.7.5508.177")]
-[assembly: AssemblyFileVersion("0.7.5508.177")]
